86b6cfa832bb875cd0a1483efb6956f2dfd5e949
Neeraj138 <neeraj.r@greyorange.com>
2025-04-30 22:15:05 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (_req: Request, res: Response) => {
+	res.send('Express Typescript on Vercel')
+    return
+})
+
+app.get('/ping', (_req: Request, res: Response) => {
+	res.send('pong 🏓')
+    return
+})
+
+app.listen(port, () => {
+	return console.log(`Server is listening on ${port}`)
+})
