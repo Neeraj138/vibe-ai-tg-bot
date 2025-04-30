7db47051fc829e053770e65e5341dae39eba9c32
Neeraj138 <neeraj.r@greyorange.com>
2025-04-30 22:37:24 +0530

    vercel changes

---
+++ backend/dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8080;
+app.get('/', (_req, res) => {
+    res.send('Express Typescript on Vercel');
+    return;
+});
+app.get('/ping', (_req, res) => {
+    res.send('pong 🏓');
+    return;
+});
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`);
+});
+//# sourceMappingURL=index.js.map
